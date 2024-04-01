7b84253d567094faf910c320d08951ee93ce94e4
Sannidhya <shubhradasgupta8@gmail.com>
2024-04-01 09:40:13 +0530

    Added datatypes and pauli gates. Working on probe func, alt of print

--- QuantaLang/gates.py
+++ QuantaLang/gates.py
@@ -1,9 +1,25 @@
+# Quantum Logic Gates
+
+
 import math
 from qubit import *
-
-
+from var import *
+from probe import *
 def hadamard(qubit_obj):
     sqrt2 = math.sqrt(2)
     alpha = (qubit_obj.alpha + qubit_obj.beta) / sqrt2
     beta = (qubit_obj.alpha - qubit_obj.beta) / sqrt2
     return Qubit(alpha, beta)
+
+def pauli_x(qubit):
+    return Qubit(qubit.beta, qubit.alpha)
+
+def pauli_y(qubit):
+    return Qubit(complex(qubit.beta.imag, -qubit.alpha.imag), complex(-qubit.beta.real, qubit.alpha.real))
+
+def pauli_z(qubit):
+    return Qubit(complex(qubit.alpha.real, -qubit.beta.imag), complex(-qubit.alpha.imag, qubit.beta.real))
+
+
+a = probe.Probe(333)
+print(a)
---
+++ QuantaLang/probe.py
@@ -0,0 +1,7 @@
+class probe:
+    class Probe:
+        def __init__(self, value):
+            self.value = value
+        
+        def __str__(self): 
+            return str(self.value)
